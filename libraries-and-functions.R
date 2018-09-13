2e1361f61c7d3b211a8ad9ec9bfba11a038d19b5
Timur Hassan <timur.hassan@gmail.com>
2018-09-13 11:30:27 +1000

    Create libraries-and-functions.R

---
+++ libraries-and-functions.R
@@ -0,0 +1,16 @@
+# Standard packages
+pacman::p_load(data.table,magrittr,sqldf,expss,dplyr,ggplot2,gridExtra,grid,lubridate,gmodels,tidyverse,questionr,scales,chron,ggforce)
+
+# function to convert decimal to percent
+percent <- function(x, digits = 2, format = "f", ...) {
+  paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+# Simple function for word wrap in titles and axis labels
+wrapper <- function(x, ...) 
+{
+  paste(strwrap(x, ...), collapse = "\n")
+}
+
+# quick csv export to tmp.csv.
+wrt <- function(data){write_csv(data,file="tmp.csv")}
