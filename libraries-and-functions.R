1f2b94dddb7ac81e62adaf8cc9d63ecdca63ff81
Timur Hassan <timur.hassan@gmail.com>
2018-09-25 09:28:42 +1000

    Added R.utils

--- libraries-and-functions.R
+++ libraries-and-functions.R
@@ -14,6 +14,7 @@ pacman::p_load(
               lubridate,
               magrittr,
               questionr,
+              R.utils,
               RCurl,
               RODBC,
               scales,
